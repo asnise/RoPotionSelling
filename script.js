7f08b6b91ddf17acca5f46a10beede980fe27860
Asnise_Git <55057145+asnise@users.noreply.github.com>
2021-11-03 20:59:09 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -22,60 +22,61 @@ function calu_count() {
     var player_zeny = document.getElementById("player_zeny").value;
     var herb_zeny = document.getElementById("herb_zeny").value;
     document.getElementById("count_hr").innerHTML = Math.floor(player_zeny / herb_zeny);
+    herb_count = Math.floor(player_zeny / herb_zeny)
+    var herb_type = document.getElementById("typeb").value;
+    compress(herb_type);
 
-     herb_count = Math.floor(player_zeny / herb_zeny)
-    
 }
 
 function calu_zeny() {
     herb_count = 0;
-     herb_count = parseInt(document.getElementById("count").value);
-     compress();
-    
+    var herb_type = document.getElementById("typea").value;
+    herb_count = parseInt(document.getElementById("count").value);
+    compress(herb_type);
+
 }
 
-function compress() {
+function compress(herb_type) {
 
-    var herb_type = document.getElementById("type");
     var price = document.getElementById("profit");
     var is_dc = document.getElementById("dc").checked;
 
-    if (is_dc && herb_count != 0) {
-        switch (herb_type.value) {
+    if (is_dc) {
+        switch (herb_type) {
             case "Yellow":
                 var sell = herb_count / 2 * 341 - 10;
                 price.innerHTML = Math.floor(herb_count / 2) + " ขวด " + " : " + Math.floor(sell) + " Zeny";
                 break;
-                
+
             case "White":
                 var sell = herb_count / 2 * 744 - 20;
                 price.innerHTML = Math.floor(herb_count / 2) + " ขวด " + " : " + Math.floor(sell) + " Zeny";
                 break;
-                
+
             case "Blue":
                 var sell = herb_count / 2 * 3100 - 30;
                 price.innerHTML = Math.floor(herb_count / 2) + " ขวด " + " : " + Math.floor(sell) + " Zeny";
                 break;
-                
+
         }
     }
-    else if (herb_count != 0) {
-        switch (herb_type.value) {
+    else {
+        switch (herb_type) {
             case "Yellow":
                 var sell = herb_count / 2 * 275 - 10;
                 price.innerHTML = Math.floor(herb_count / 2) + " ขวด " + " : " + Math.floor(sell) + " Zeny";
                 break;
-                
+
             case "White":
                 var sell = herb_count / 2 * 600 - 20;
                 price.innerHTML = Math.floor(herb_count / 2) + " ขวด " + " : " + Math.floor(sell) + " Zeny";
                 break;
-                
+
             case "Blue":
                 var sell = herb_count / 2 * 2500 - 30;
                 price.innerHTML = Math.floor(herb_count / 2) + " ขวด " + " : " + Math.floor(sell) + " Zeny";
                 break;
-                
+
         }
     }
 }
